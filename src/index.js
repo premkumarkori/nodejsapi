fa25f82d0502205e2099eba00f7b6ac9309a214b
premkumarkori <premkumar.kori@mavs.uta.edu>
2022-07-04 15:41:24 -0500

    Fifth Commit: Added Routing

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import Response from './domain/response.js';
 import logger from './util/logger.js';
 import HttpStatus from './controller/patient.controller.js';
+import patientRoutes from './routes/patient.route.js'
 
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 3000;
@@ -12,7 +13,11 @@ const app = express();
 app.use(cors({ origin: '*'}));
 app.use(express.json())
 
+app.use('/patients', patientRoutes);
 app.get('/', (req, res)=>res.send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Patient API, v1.0.0')));
+app.get('*', (req, res)=>res.status(HttpStatus.NOT_FOUND.code)
+.send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, 'Route Does not exists')));
+
 app.listen(PORT,()=>{
     logger.info(`Server running on: ${ip.address()}:${PORT}`)
 })
