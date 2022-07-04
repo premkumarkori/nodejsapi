bbbc3995961837207e0199d10a7d638d1123ee1d
premkumarkori <premkumar.kori@mavs.uta.edu>
2022-07-04 11:35:50 -0500

    First Commit: Setting up NodeJS Server

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import ip from 'ip';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const PORT = process.env.SERVER_PORT || 3000;
+const app = express();
+app.use(cors({ origin: '*'}));
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send({ message: 'UP'})
+})
+console.log(process.env);
+app.listen(PORT,()=>{
+    console.log(`Server running on: ${ip.address()}:${PORT}}`)
+})
